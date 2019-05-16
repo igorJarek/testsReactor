c111bccf99e8f8b583af46a8c4ca0157a6414ef7
201953 <201953@edu.p.lodz.pl>
2019-05-16 16:50:57 +0200

    initial test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,15 +1,28 @@
 package edu.iis.mto.testreactor.exc3;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AtmMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
+    @Before
+    public void setUp() {
+        CardProviderService cardProviderService = mock(CardProviderService.class);
+        BankService bankService = mock(BankService.class);
+        MoneyDepot moneyDepot = mock(MoneyDepot.class);
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+
+        Money widthdrawMoney = Money.builder().withAmount(10).withCurrency(Currency.PL).build();
+        Card.builder().withCardNumber("123456").withPinNumber(1234).build();
     }
 
+    @Test
+    public void test() {
+
+    }
 }
